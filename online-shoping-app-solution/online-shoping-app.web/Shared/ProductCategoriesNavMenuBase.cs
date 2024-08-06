da0d1cd753ebc0026796835e4ea52bca90c38a4c
Kamilek <radek71k@onet.pl>
2024-08-06 15:08:55 +0200

    Category navigation menu

---
+++ online-shoping-app-solution/online-shoping-app.web/Shared/ProductCategoriesNavMenuBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using online_shoping.Models.Dtos;
+using online_shoping_app.web.Interfaces;
+
+namespace online_shoping_app.web.Shared
+{
+    public class ProductCategoriesNavMenuBase : ComponentBase
+    {
+        [Inject]
+        public IProductService ProductService { get; set; }
+        public IEnumerable<ProductCategoryDto> ProductCategoryDtos { get; set; }
+        public string ErrorMessage { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                ProductCategoryDtos = await ProductService.GetProductCategories();
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+            }
+        }
+    }
+}
