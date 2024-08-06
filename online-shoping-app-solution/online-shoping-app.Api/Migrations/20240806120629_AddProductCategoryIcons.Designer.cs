a4b6b03c8df8541dca01f15a71576d64f9690451
Kamilek <radek71k@onet.pl>
2024-08-06 14:08:28 +0200

    ProductCategory now has icons assigned to then

--- online-shoping-app-solution/online-shoping-app.Api/Migrations/20240806120629_AddProductCategoryIcons.Designer.cs
+++ online-shoping-app-solution/online-shoping-app.Api/Migrations/20240806120629_AddProductCategoryIcons.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using online_shoping_app.Api.Data;
 
@@ -10,9 +11,10 @@ using online_shoping_app.Api.Data;
 namespace online_shoping_app.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240806120629_AddProductCategoryIcons")]
+    partial class AddProductCategoryIcons
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
