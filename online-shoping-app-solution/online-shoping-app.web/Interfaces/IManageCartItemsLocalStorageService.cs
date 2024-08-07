c22a93dd3de86b08dd8740b4b58fb8818e18370c
Kamilek <radek71k@onet.pl>
2024-08-07 16:59:38 +0200

    Service to manage Cart items in local storage

---
+++ online-shoping-app-solution/online-shoping-app.web/Interfaces/IManageCartItemsLocalStorageService.cs
@@ -0,0 +1,11 @@
+﻿using online_shoping.Models.Dtos;
+
+namespace online_shoping_app.web.Interfaces
+{
+    public interface IManageCartItemsLocalStorageService
+    {
+        Task<List<CartItemDto>> GetCollection();
+        Task SaveCollection(List<CartItemDto> cartItemDtos);
+        Task RemoveCollection();
+    }
+}
--- online-shoping-app-solution/online-shoping-app.web/Program.cs
+++ online-shoping-app-solution/online-shoping-app.web/Program.cs
@@ -3,13 +3,19 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using online_shoping_app.web;
 using online_shoping_app.web.Interfaces;
 using online_shoping_app.web.Services;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7022/") });
+
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IShoppingCartService, ShoppingCartService>();
+builder.Services.AddScoped<IManageCartItemsLocalStorageService, ManageCartItemsLocalStorageService>();
+builder.Services.AddScoped<IManageProductsLocalStorageService, ManageProductsLocalStorageService>();
+
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
