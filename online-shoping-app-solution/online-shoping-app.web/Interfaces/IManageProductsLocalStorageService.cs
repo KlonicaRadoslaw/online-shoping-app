abe9e1cf62eb45d71cb975a1bad0e96675d06eda
Kamilek <radek71k@onet.pl>
2024-08-07 16:49:17 +0200

    Service to manage Products in local storage

---
+++ online-shoping-app-solution/online-shoping-app.web/Interfaces/IManageProductsLocalStorageService.cs
@@ -0,0 +1,10 @@
+﻿using online_shoping.Models.Dtos;
+
+namespace online_shoping_app.web.Interfaces
+{
+    public interface IManageProductsLocalStorageService
+    {
+        Task<IEnumerable<ProductDto>> GetCollection();
+        Task RemoveCollection();
+    }
+}
